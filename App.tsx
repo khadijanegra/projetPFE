a93d4210d7e89bb92e3522fdeb5b95c182e07809
khadijanegra <khadijanegra2@gmail.com>
2025-02-05 21:43:46 +0100

    suppression de name

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-
-
+import Authentication from './authefication';
 import Login from './login';
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
- <Login></Login>
-      
+ <Login></Login> 
+  
+   
     </SafeAreaView>
   );
 };
--- login.tsx
+++ login.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import CheckBox from '@react-native-community/checkbox';
 import tw from 'tailwind-react-native-classnames';
 
 export default function Login() {
@@ -9,21 +8,18 @@ export default function Login() {
 
   return (
     <SafeAreaView style={tw`flex-1 bg-white`}> 
-      <View style={tw`flex-1 items-center justify-center px-6`}> 
-        <Text style={tw`text-2xl font-bold mb-6`}>Sign Up</Text>
+      <View style={tw`items-center justify-center flex-1 px-6`}> 
+        <Text style={tw`mb-6 text-2xl font-bold`}>Sign Up</Text>
         
-        <TextInput
-          placeholder="Name"
-          style={tw`w-full h-12 border border-gray-300 rounded-lg px-4 mb-4`}
-        />
+      
         <TextInput
           placeholder="Email address"
-          style={tw`w-full h-12 border border-gray-300 rounded-lg px-4 mb-4`}
+          style={tw`w-full h-12 px-4 mb-4 border border-gray-300 rounded-lg`}
         />
         <TextInput
           placeholder="Password"
           secureTextEntry
-          style={tw`w-full h-12 border border-gray-300 rounded-lg px-4 mb-4`}
+          style={tw`w-full h-12 px-4 mb-4 border border-gray-300 rounded-lg`}
         />
 
         <View style={tw`flex-row items-center mb-4`}> 
@@ -31,11 +27,11 @@ export default function Login() {
           <Text style={tw`text-gray-700`}>I accept Privacy Policy + Terms of Use</Text>
         </View>
 
-        <TouchableOpacity style={tw`w-full h-12 bg-yellow-500 rounded-lg items-center justify-center mb-6`}>
-          <Text style={tw`text-white text-lg font-bold`}>Create Account</Text>
+        <TouchableOpacity style={tw`items-center justify-center w-full h-12 mb-6 bg-yellow-500 rounded-lg`}>
+          <Text style={tw`text-lg font-bold text-white`}>Create Account</Text>
         </TouchableOpacity>
 
-        <Text style={tw`text-gray-500 mb-4`}>Or continue with</Text>
+        <Text style={tw`mb-4 text-gray-500`}>Or continue with</Text>
 
         <View style={tw`flex-row justify-center`}> 
           <TouchableOpacity style={tw`mx-2`}>
