ee2c1017005a29f0957bf5d8dcd3d446d6278e82
Marambenattallah <maram.benattallah3@gmail.com>
2025-03-05 15:26:57 +0100

    ajout page favourite

--- screens/home/userfavoris.tsx
+++ screens/home/userfavoris.tsx
@@ -1,28 +1,41 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import tw from "tailwind-react-native-classnames";
 
 const TaskCard = () => {
-  const statusColors = {
-    Waiting: "bg-blue-500",
-    Suspended: "bg-purple-600",
-    "In Progress": "bg-yellow-500",
-  };
-
   return (
-    <View style={tw`p-4 m-2 bg-red-100 border border-gray-200 shadow-lg rounded-2xl`}> 
-      <View style={tw`flex-row items-center mb-2`}>
-        <Image
-          source={{ uri: "https://via.placeholder.com/40" }}
-          style={tw`w-10 h-10 mr-3 rounded-full`}
-        />
-        <Text style={tw`font-bold text-gray-800 te-xt-lg `}>Title lorem ipsum</Text>
+    <View style={tw`p-4 m-2 bg-white border border-gray-300 shadow-lg rounded-2xl`}> 
+      {/* Image principale */}
+      <Image
+        source={require("../../images/cafe.png")} // Remplace par l'URL de l'image réelle
+        style={tw`w-full h-40 rounded-xl mb-2`}
+      />
+      
+      {/* Localisation */}
+      <View style={tw`flex-row items-center mb-1`}> 
+        <Text style={tw`px-2 py-1 text-xs font-bold text-white bg-red-300 rounded-full`}>Kairouan</Text>
+        
       </View>
-      <Text style={tw`mb-2 text-gray-600`}>It is possible to place this data on the Single Page or the items grid. You will be able to do it only </Text>
-      <Text style={tw`text-sm text-gray-500`}>Deadline</Text>
-      <Text style={tw`mb-2 font-semibold text-black`}>24/04/2003</Text>
-      <View style={tw`self-end px-4 py-1 rounded-full `}>
-        <Text style={tw`font-semibold text-white`}>aaaaaaa</Text>
+      
+      {/* Titre du restaurant */}
+      <Text style={tw`mb-1 text-lg font-bold text-gray-800`}>Secret garden</Text>
+      
+      {/* Prix moyen */}
+      
+      
+      {/* Note et avis */}
+      <View style={tw`flex-row items-center justify-between mt-2`}>
+        <View style={tw`flex-row items-center`}>
+          <View style={tw`px-2 py-1 bg-red-300 rounded-lg`}> 
+            <Text style={tw`text-white font-bold text-sm`}>3.4</Text>
+          </View>
+          <Text style={tw`ml-2 text-gray-600 text-sm`}>Bien (32)</Text>
+        </View>
+        
+        {/* Icône favoris */}
+        <TouchableOpacity>
+          <Text style={tw`text-xl`}>🤍</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
